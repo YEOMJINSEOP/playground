e124ff1b3ad48d1ed1a52cf6edab029e1151a42a
jinseop <yeomjinseop@gmail.com>
2022-09-23 22:38:36 +0900

    React: Complete Habit-Tracker

---
+++ habit-tracker/.history/src/components/simpleHabit_20220923104445.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const SimpleHabit = (props) => {
+  const [ count, setCount] = useState(0);
+  return(
+    <li className="habit">
+      <span className="habit-name">Reading</span>
+      <span className="habit-count">{this.state.count}</span>
+      <button
+        className="habit-button habit-increase"
+        onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+    </li>
+  );
+};
+
+
+export default SimpleHabit;
---
+++ habit-tracker/.history/src/components/simpleHabit_20220923105515.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const SimpleHabit = (props) => {
+  const [ count, setCount] = useState(0);
+  const spanRef = useRef();
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(()=>{
+    console.log(`mounted & updated!: ${count}`);
+  }, [count]);
+
+  return(
+    <li className="habit">
+      <span className="habit-name">Reading</span>
+      <span className="habit-count">{count}</span>
+      <button
+        className="habit-button habit-increase"
+        onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+    </li>
+  );
+};
+
+
+export default SimpleHabit;

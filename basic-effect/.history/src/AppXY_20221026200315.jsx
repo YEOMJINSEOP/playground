e9e82211f92ca2af7eca2943fcd9b25b6629c774
jinseop <yeomjinseop@gmail.com>
2022-10-26 23:01:05 +0900

    React Basic Practice

---
+++ basic-effect/.history/src/AppXY_20221026200315.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AppXY.css';
+
+function AppXY(props) {
+  return (
+    <div className='container'>
+      <div className='pointer'></div>
+    </div>
+  );
+}
+
+export default AppXY;   

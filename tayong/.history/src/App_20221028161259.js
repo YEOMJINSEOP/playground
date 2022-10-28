36e781a13c4593f6bb659b18640cf68551985bd1
jinseop <yeomjinseop@gmail.com>
2022-10-28 16:16:19 +0900

    Tayong:Fix Error

---
+++ tayong/.history/src/App_20221028161259.js
@@ -0,0 +1,16 @@
+import styles from './app.module.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import LocationSelector from './components/locationSelector/locationSelector';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <LocationSelector/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;

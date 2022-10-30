cd65b7f14faf06d21af1227ff622ee403b53d401
jinseop <yeomjinseop@gmail.com>
2022-10-30 09:54:11 +0900

    Tayong: Import UUID

--- tayong/.history/src/components/list/meetList_20221030095123.jsx
+++ tayong/.history/src/components/list/meetList_20221030095123.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
-import {v4 as uuidV4} from 'uuid';
 
 function MeetList(props) {
   const [meetList, setMeetList] = useState([
-    { 'id': uuidV4(),
+    { 
       'destination': '디지털미디어시티',
       'arrival': '한국항공대학교',
       'recruitment': '1/4',
       'remainingTime': '1시간 27분 남음'
     },
     {
-      'id': uuidV4(),
       'destination': '디지털미디어시티',
       'arrival': '한국항공대학교',
       'recruitment': '3/4',
@@ -26,7 +24,7 @@ function MeetList(props) {
         {meetList.map((item) => {
           console.log('hi');
           console.log(item);
-          return <li key={item.id}>{item.destination}, {item.arrival}, {item.recruitment}, {item.remainingTime}</li>
+          return <li key={new Date()}>{item.destination}, {item.arrival}, {item.recruitment}, {item.remainingTime}</li>
         })}
       </ul>
     </div>
